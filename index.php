5015efeae1b37f018a9cff59b034f4c0e266492d
Sir mXe <warman.t.gary@gmail.com>
2014-03-12 12:18:15 -0600

    added markdown library changed directory structure linked to cdn hosted kinetic and jquery

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <html>
   <head>
     <title>YOU SHOULD NOT SEE THIS</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="res/css/main.css">
   </head>
   <body>
     DOCUMENTATION FOR SOME SHIT
-    <script src="lib/markdown.js"></script>
+    <script src='/res/js/markdown.js'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/kineticjs/5.0.1/kinetic.min.js'></script>
+    script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <canvas></canvas>
   </body>
 </html>
