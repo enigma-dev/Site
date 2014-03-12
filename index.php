452ef734a9984b750540f617eab29147ea17d72e
Sir mXe <warman.t.gary@gmail.com>
2014-03-12 12:47:16 -0600

    added some shit

--- index.php
+++ index.php
@@ -4,10 +4,28 @@
     <link rel="stylesheet" href="res/css/main.css">
   </head>
   <body>
-    DOCUMENTATION FOR SOME SHIT
-    <script src='/res/js/markdown.js'></script>
+    DOCUMENTATION FOR SOME SHIT<br />
+    FUCKING CAN I GET SOME YAML OR SOME SHIT PLS
+    <form>
+	<textarea id="yaml" placeholder="YAML goes here." class='yaml-container' autofocus></textarea>
+    </form>
+    <input type='button' value='DOC ME PLS' />
+    <div id='container'>
+    </div>
+    <script src='res/js/js-yaml.min.js'></script>
+    <script src='res/js/markdown.js'></script>
+    <script src='res/js/wysi-parser-advanced.js'></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/wysihtml5/0.3.0/wysihtml5.min.js'></script>
     <script src='//cdnjs.cloudflare.com/ajax/libs/kineticjs/5.0.1/kinetic.min.js'></script>
-    script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
-    <canvas></canvas>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script src='//cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.0/js/toastr.min.js'></script>
+    <script>
+	$(function() {
+		var editor = new wysihtml5.Editor("yaml", { // id of textarea element
+		  parserRules:  wysihtml5ParserRules // defined in parser rules set 
+		});
+		
+	})
+    </script>
   </body>
 </html>
