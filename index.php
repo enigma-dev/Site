57e489f9f222216813b4a5941e29bf46588bdef8
Sir mXe <warman.t.gary@gmail.com>
2014-03-12 12:12:31 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>YOU SHOULD NOT SEE THIS</title>
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
+    DOCUMENTATION FOR SOME SHIT
+    <script src="lib/markdown.js"></script>
+    <canvas></canvas>
+  </body>
+</html>
